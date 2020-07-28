7a4aae0824ea1782e1d77687857eae669f548816
gu <1982775886@qq.com>
2020-07-28 15:30:02 +0800

    猜数字游戏

--- src/main.go
+++ src/main.go
@@ -1,7 +1,39 @@
+// 猜数字游戏
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func guess(dst int) {
+	for {
+		fmt.Print("请猜1000以内自然数: ")
+		var num int
+		fmt.Scanf("%d", &num)
+		fmt.Println(num)
+		if num > dst {
+			fmt.Println("大了")
+		} else if num < dst {
+			fmt.Println("小了")
+		} else {
+			fmt.Println("猜对了!")
+			break
+		}
+	}
+}
+
+func game() {
+	rand.Seed(time.Now().UnixNano())
+	var dst int
+	for {
+		dst = rand.Intn(1000)
+		fmt.Println("我已经想好了一个数字,你猜吧")
+		guess(dst)
+	}
+}
 
 func main() {
-	fmt.Printf("你好,我是鸡毛\n")
+	game()
 }
