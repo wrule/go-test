e55c58b636ed8468348cc5c8791f0c8bbd1b76a1
jimao <1982775886@qq.com>
2020-08-10 15:51:51 +0800

    提交代码

--- src/main.go
+++ src/main.go
@@ -1,13 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "fmt"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	var list []int
+	list = append(list, 2)
+	fmt.Println(list[0], len(list), cap(list))
 }
